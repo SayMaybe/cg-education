1176ca5b7cf4c4abe8880affa85d25cd582fd75b
Rebecca <say.maybe@yahoo.com>
2017-09-10 16:01:52 -0500

    teacher box changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("This is a file.");
+var firstName = 'Betsy';
+var lastName = 'Jones';
+var teacherName = firstName + lastName;
+console.log(teacherName);
+var teacherList = [{firstName: 'Betsy', lastName: 'Jones', department: 'Physics'},
+                    {firstName: 'John', lastName: 'Fleming', department: 'Astronomy'}];
+/*var teacher = {};
+teacher.firstName = 'Betsy';
+teacher.lastName = 'Jones';
+teacher.department = 'Physics';
+teacherList.push(teacher);
+teacher = {};
+teacher.firstName = 'John';
+teacher.lastName = 'Fleming';
+teacher.department = 'Astronomy';
+teacherList.push(teacher);*/
+console.log(teacherList);
