47f53b46fde821af748680110eb224c5a86f0acb
Say Maybe <say.maybe@yahoo.com>
2017-09-11 21:27:17 -0500

    js updated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 console.log("This is a file.");
 var firstName = 'Betsy';
 var lastName = 'Jones';
-var teacherName = firstName + lastName;
+var teacherName = firstName + " " + lastName;
 console.log(teacherName);
-var teacherList = [{firstName: 'Betsy', lastName: 'Jones', department: 'Physics'},
+/*var teacherList = [{firstName: 'Betsy', lastName: 'Jones', department: 'Physics'},
                     {firstName: 'John', lastName: 'Fleming', department: 'Astronomy'}];
 /*var teacher = {};
 teacher.firstName = 'Betsy';
@@ -14,5 +14,14 @@ teacher = {};
 teacher.firstName = 'John';
 teacher.lastName = 'Fleming';
 teacher.department = 'Astronomy';
-teacherList.push(teacher);*/
-console.log(teacherList);
+teacherList.push(teacher);
+console.log(teacherList);*/
+var department = 'Physics';
+var ratingOne = 1.25;
+var ratingTwo = 5.0;
+var ratingThree = 3.6;
+var avgRating = (ratingOne + ratingTwo + ratingThree) / 3;
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + ratingOne + ratingTwo + ratingThree);
+console.log("Average Rating: " + Math.round(avgRating * 100) / 100);
