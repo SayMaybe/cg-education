7947a18593f5e4ea826c858c54dc67cb1380068c
Rebecca <say.maybe@yahoo.com>
2017-09-26 22:45:41 -0500

    JS ratings function

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
-/*console.log("This is a file.");
-var teacherFirstName = 'Betsy';
-var teacherLastName = 'Jones';
-var teacherName = teacherFirstName + " " + teacherLastName;
+console.log("This is a file.");
+var teacherName = 'Betsy Miller';
+var teacherDepartment = 'Physics';
 console.log(teacherName);
-/*var teacherList = [{firstName: 'Betsy', lastName: 'Jones', department: 'Physics'},
-                    {firstName: 'John', lastName: 'Fleming', department: 'Astronomy'}];
-/*var teacher = {};
-teacher.firstName = 'Betsy';
-teacher.lastName = 'Jones';
-teacher.department = 'Physics';
-teacherList.push(teacher);
-teacher = {};
-teacher.firstName = 'John';
-teacher.lastName = 'Fleming';
-teacher.department = 'Astronomy';
-teacherList.push(teacher);
-console.log(teacherList);
-var department = 'Physics';
+console.log(teacherDepartment);
 var ratingOne = 1.25;
 var ratingTwo = 5.0;
 var ratingThree = 3.6;
-var avgRating = (ratingOne + ratingTwo + ratingThree) / 3;
-console.log("Teacher: " + teacherName);
-console.log("Department: " + department);
-console.log("Ratings: " + ratingOne + ratingTwo + ratingThree);
-console.log("Average Rating: " + Math.round(avgRating * 100) / 100);
-var studentFirstName = 'Joe';
-var studentLastName = 'Camel';
-var studentName = studentFirstName + " " + studentLastName;
-console.log(studentName);
+/*var avgRating = (ratingOne + ratingTwo + ratingThree) / 3;
+avgRating = Math.round(avgRating * 10) / 10;*/
+var teacherRatings = [1.25, 5.0, 5.5];
+console.log(teacherRatings);
+function getRatingAvg(teacherRatings) {
+  var totalRatings = 0;
+  for (var i = 0; i < teacherRatings.length; i++) {
+      totalRatings = totalRatings + teacherRatings[i];
+  }
+  return Math.round((totalRatings / teacherRatings.length) * 10) / 10;
+}
+console.log(getRatingAvg (teacherRatings));
+
+/*console.log(studentName);
 var major = 'Astrology';
 var email = 'jos.camel@esp.com';
 var gpa = '3.2';
