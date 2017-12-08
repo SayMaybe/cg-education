b4e3ca12795407be68a22e580494b3af12049e45
Rebecca Heermann <rheermann@chausa.org>
2017-12-08 11:34:36 -0600

    teacherObject.js

---
+++ js/teacherObjects.js
@@ -0,0 +1,38 @@
+var teacher = {
+  name: "The Colonel", department: "International Studies",
+  ratings: [3.0, 2.5, 1.9],
+  addRating: function(newRating) {
+    this.ratings.push(newRating);
+    },
+  getRatingAvg: function() {
+    var totalRatings = 0;
+    for (var i = 0; i < this.ratings.length; i++) {
+      totalRatings = totalRatings + this.ratings[i];
+    }
+    return Math.round((totalRatings / this.ratings.length) * 10) / 10;
+  },
+
+  validRating: function(newRating) {
+    if (newRating >= 1.0 && newRating <= 5.0){
+      this.addRating(newRating);
+      
+    }
+    return false;
+  }
+};
+console.log(teacher.ratings);
+
+var shouldIRateATeacher = true;
+
+while (shouldIRateATeacher){
+  var newRating = 0;
+   while (!teacher.validRating(newRating)){
+     newRating = prompt("Please give " + teacher.name + " a rating between 1.0 and 5.0");
+     newRating = newRating * 1.0;
+     if (!teacher.validRating(newRating)){
+       alert("I said a number between 1.0 and 5.0! Press enter to try again.");
+     };
+   };
+shouldIRateATeacher = false;
+ };
+console.log(teacher.ratings);
