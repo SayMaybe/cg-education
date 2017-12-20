2b61bc3a41550363620340fd5fa03ca636647cee
Rebecca <say.maybe@yahoo.com>
2017-12-20 13:28:21 -0600

    course and teacher objects created

--- js/coursesObject.js
+++ js/coursesObject.js
@@ -1,26 +1,27 @@
+console.log("I am linked.");
+
 var course = {
 name: "Junk Shopping",
 department: "International Studies",
 teacher: "The Colonel",
 semester: "Fall 2017",
-validDept = function(deptSearched) {
-  if (deptSearched == "International Studies" || deptSearched == "Music" || deptSearched == "Mathematics" || deptSearched == "Meteorology" || deptSearched == "Women's Studies") {
+validDept: function(deptSearched) {
+  if (deptSearched == this.department) {
     return true;
-  }
+  } else {
     return false;
+    }
   }
 }
-var deptSearched = false;
-while (!validDept(deptSearched)){
-  deptSearched = prompt("Please enter a department in which to search for a class.")
-    if (!validDept(deptSearched)){
+
+var deptSearched = "";
+
+if (!course.validDept(deptSearched)){
+  deptSearched = prompt("Please enter a department to search for a class.")
+    if (!course.validDept(deptSearched)){
       alert("Your choice does not have any classes.");
     }
     else {
-        for (var i = 0; i < course.length; i++){
-          if (course.department == deptSearched){
-
-    }
-  }
-      alert("The classes are " + course.department + ".")
-}
+            alert("The classes are " + course.department + ".");
+          }
+      }
--- js/teacherObject.js
+++ js/teacherObject.js
@@ -40,3 +40,30 @@ teacher.addRating(newRating);
 shouldIRateATeacher = false;
  };
 console.log(teacher.ratings);
+
+var course = {
+name: "Junk Shopping",
+department: "International Studies",
+teacher: teacher.name,
+semester: "Fall 2017",
+validDept: function(deptSearched) {
+  if (deptSearched == this.department) {
+    return true;
+  } else {
+    return false;
+    }
+  }
+}
+console.log(course.teacher.name);
+
+var deptSearched = "";
+
+if (!course.validDept(deptSearched)){
+  deptSearched = prompt("Please enter a department to search for a class.")
+    if (!course.validDept(deptSearched)){
+      alert("Your choice does not have any classes.");
+    }
+    else {
+            alert("The classes are " + course.department + ".");
+          }
+      }
